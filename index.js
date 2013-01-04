d6a2aafcc44b35d5fdb1fd48b2a4aa2fd77dbf82
Toshiyuki Nakamura <yukidarake4@gmail.com>
2013-01-04 13:16:04 +0900

    handle remote-files

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var fs = require('fs');
+
+var async = require('async');
 var difflet = require('difflet');
 var deepEqual = require('deep-equal');
+var request = require('request');
 
 if (process.argv.length < 4) {
     console.log('usage: chau a.json b.json');
+    console.log('   or: chau http://example.com/a.json http://example.com/b.json');
     process.exit(1);
 }
 
-var read = function(file) {
+var parse = function(data) {
     try {
-        var lines = fs.readFileSync(file, 'utf8')
+        var lines = (data || '')
             .split(/\n/)
             .filter(function(line) {
                 return line;
@@ -26,24 +30,55 @@ var read = function(file) {
         }, {});
 
     } catch(e) {
-        return JSON.parse(fs.readFileSync(file, 'utf8'));
+        return JSON.parse(data);
     }
 };
+
+var read = function(path, callback) {
+    if (/^http/.test(path)) {
+        request(path, function(err, res, body) {
+            if (err) {
+                throw err;
+            }
+
+            callback(null, parse(body));
+        });
+        return;
+    }
+
+    fs.readFile(path, 'utf8', function(err, data) {
+        if (err) {
+            throw err;
+        }
+
+        callback(null, parse(data));
+    });
+};
+
 
 var diff = difflet({
     indent:2,
     comment: true
 });
 
-var prev = read(process.argv[2]);
-var next = read(process.argv[3]);
+async.parallel({
+    prev: function(next) {
+        read(process.argv[2], next);
+    },
+    next: function(next) {
+        read(process.argv[3], next);
+    },
+}, function(err, data) {
+    var prev = data.prev;
+    var next = data.next;
 
-Object.keys(prev).forEach(function(key) {
-    if (deepEqual(prev[key], next[key])) {
-        delete prev[key];
-        delete next[key];
-    }
+    Object.keys(prev).forEach(function(key) {
+        if (deepEqual(prev[key], next[key])) {
+            delete prev[key];
+            delete next[key];
+        }
+    });
+
+    console.log(diff.compare(prev, next));
 });
-
-console.log(diff.compare(prev, next));
 
